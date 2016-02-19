6349b4bdaea84fe1352e06d3665c02d38119c9ef
i418c <starfield94@aol.com>
2016-02-18 20:59:24 -0800

    File System and some parsing options.

--- CosmosKernel1/FileSystem.cs
+++ CosmosKernel1/FileSystem.cs
@@ -8,5 +8,33 @@ namespace CosmosKernel1
 {
     class FileSystem
     {
+        static System.Collections.ArrayList files;
+
+        public FileSystem()
+        {
+            if (files==null)
+            {
+                files = new System.Collections.ArrayList();
+            }
+        }
+
+        public void create(string name, string extension)
+        {
+            files.Add(new File(name, extension));
+        }
+        
+        public string[] list()
+        {
+            string[] filenames = new string[files.Count];
+            var ct = 0;
+            foreach (var file in files)
+            {
+                filenames[ct] = String.Format("--{0,10}--", file.getName(), file.getExtension(), file.getDateCreated(), file.getFileSize();
+                ct++;
+            }
+            return filenames;
+        }
+
+
     }
 }
--- CosmosKernel1/Kernel.cs
+++ CosmosKernel1/Kernel.cs
@@ -7,9 +7,12 @@ namespace CosmosKernel1
 {
     public class Kernel : Sys.Kernel
     {
+        private FileSystem fs;
+
         protected override void BeforeRun()
         {
             Console.WriteLine("Cosmos booted successfully. Type help for a list of commands.");
+            fs = new FileSystem();
         }
 
         protected override void Run()
@@ -32,6 +35,28 @@ namespace CosmosKernel1
                         Console.Write(tokens[ct]+" ");
                     }
                     Console.WriteLine("");
+                    break;
+                case "create":
+                    var args = tokens[1].Split('.');
+                    fs.create(args[0],args[1]);
+                    break;
+                case "dir":
+                case "ls":
+                    string[] filenames = fs.list();
+                    foreach(var filename in filenames)
+                    {
+                        Console.WriteLine(filename);
+                    }
+                    break;
+                case "set":
+                    if (!System.Text.RegularExpressions.Regex.Match(tokens[1],"^[$]").Success)
+                    {
+                        Console.WriteLine("Variables must start with $");
+                    }
+                    else
+                    {
+
+                    }
                     break;
                 default:
                     Console.WriteLine("Unkown Command: " + tokens[0]);
